819e4df156996d1bc3e5eb0d8a1ad8d7efa7dbc0
ExpDevStar <expdevstar000@outlook.com>
2014-04-04 13:16:51 -0700

    Reduce the handleRegister and handleLogin functions into one.
    Use .bind to pass in parameter into the callback function.

--- app/assets/javascripts/react-components/_welcome_page.js.jsx
+++ app/assets/javascripts/react-components/_welcome_page.js.jsx
@@ -2,23 +2,18 @@
 
 var WelcomePage = React.createClass({
 
-	/*This handles the event where a user clicks register*/
-	handleRegister: function(event) {
-		var register_page = 1;
-		this.props.onWelcomeClick(register_page);
+	/*This handles the event where a user clicks either the Register or Login button.
+	Register = 1 and Login = 2*/
+	handleClick: function(page_num) {
+		this.props.onWelcomeClick(page_num);
 	},
 
-	/*This handles the event where a user clicks login*/
-	handleLogin: function(event) {
-		var login_page = 2;
-		this.props.onWelcomeClick(login_page);
-	},
 	render: function() {
 		return (
 			<div className="center-content">
 				<p className="welcome-text">Welcome</p>
-				<div className="welcome-button button_link" onClick={this.handleRegister}>Register</div>
-				<div className="welcome-button button_link" onClick={this.handleLogin}>Login</div>
+				<div className="welcome-button button_link" onClick={this.handleClick.bind(this, 1)}>Register</div>
+				<div className="welcome-button button_link" onClick={this.handleClick.bind(this, 2)}>Login</div>
 			</div>
 		);
 	}
